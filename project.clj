a8667dce7a22582f1ecbfa417e3de7e4df8b7152
Tomato <Tomato.HF@gmail.com>
2014-11-07 17:45:44 +0800

    util to read files

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   reviewer "0.1.0"
   :description "naive code reviewer"
   :main qiaobutang.reviewer.main
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [me.raynes/fs "1.4.5"]])
---
+++ src/qiaobutang/reviewer/util.clj
@@ -0,0 +1,10 @@
+(ns qiaobutang.reviewer.util
+  (:require [me.raynes.fs :as fs]
+            [clojure.java.io :as io]))
+
+(defn files [dir, extension]
+  (fs/find-files dir (re-pattern (str ".+\\." extension))))
+
+(defn lines [file]
+  (with-open [rdr (io/reader file)]
+    (doall (line-seq rdr))))
