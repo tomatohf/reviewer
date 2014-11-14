ba08bc41cf0e106edf3b2453c83333a8dc978ef5
Tomato <Tomato.HF@gmail.com>
2014-11-14 16:03:31 +0800

    run and print result

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.namespace "0.2.7"]
                  [org.clojure/java.classpath "0.2.2"]
+                 [clansi "1.0.0"]
                  [me.raynes/fs "1.4.5"]])
--- src/qiaobutang/reviewer/main.clj
+++ src/qiaobutang/reviewer/main.clj
@@ -1,8 +1,8 @@
 (ns qiaobutang.reviewer.main
   (:require [clojure.tools.namespace.find :as ns]
-            [clojure.java.classpath :as cp]))
-
-(defn -main [& args])
+            [clojure.java.classpath :as cp]
+            clansi
+            [clojure.string :as string]))
 
 (defn task-ns []
   (filter #(.startsWith (str %) "qiaobutang.reviewer.task.")
@@ -11,3 +11,41 @@
 (defn task [ns]
   (require ns)
   (ns-resolve ns 'run))
+
+(defmacro print-nested-result [result empty-seq rest-empty-var vars & body]
+ `(loop [[~vars & rest-result#] ~result]
+    (let [rest-empty# (empty? rest-result#)
+          ~rest-empty-var rest-empty#]
+      (doseq [empty# ~empty-seq]
+        (print (if empty# " " "│"))
+        (print "   "))
+      (print (if rest-empty# "└" "├"))
+      (print "── ")
+      ~@body
+      (if-not rest-empty# (recur rest-result#)))))
+
+(defn print-result [ns result]
+  (when (seq result)
+    (println (clansi/style ns :bg-blue))
+    (print-nested-result result
+                         []
+                         rest-files-empty
+                         [file file-result]
+      (println (clansi/style (.getCanonicalPath file) :red))
+      (print-nested-result file-result
+                           [rest-files-empty]
+                           rest-lines-empty
+                           [n line line-result]
+        (println (clansi/style (str "#" n) :red) (clansi/style (string/trim line) :magenta))
+        (print-nested-result line-result
+                           [rest-files-empty rest-lines-empty]
+                           _
+                           problem
+          (println (clansi/style problem :yellow)))))))
+
+(defn -main
+  ([] (-main "."))
+  ([path & _]
+    (doseq [ns (task-ns)]
+      (when-let [f (task ns)]
+        (print-result ns (f path))))))
