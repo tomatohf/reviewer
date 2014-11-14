59a8bd4431b7796ddccacd323919406b5f155f4b
Tomato <Tomato.HF@gmail.com>
2014-11-14 16:11:12 +0800

    init task for scala

---
+++ src/qiaobutang/reviewer/task/scala.clj
@@ -0,0 +1,14 @@
+(ns qiaobutang.reviewer.task.scala
+  (:require [qiaobutang.reviewer.util :as util]))
+
+(defn review-line [n line lines]
+  (util/not-empty-with []
+                       (+ n 1) line))
+
+(defn review-file [file]
+  (let [lines (vec (util/lines file))]
+    (util/not-empty-with (map-indexed #(review-line %1 %2 lines) lines)
+                         file)))
+
+(defn run [path]
+  (util/not-empty-with (map review-file (util/files path "scala"))))
--- src/qiaobutang/reviewer/util.clj
+++ src/qiaobutang/reviewer/util.clj
@@ -8,3 +8,13 @@
 (defn lines [file]
   (with-open [rdr (io/reader file)]
     (doall (line-seq rdr))))
+
+(defmacro not-empty-let [bindings & body]
+  (let [[name coll] bindings]
+   `(let [~name (filter (complement nil?) ~coll)]
+      (when (seq ~name)
+        ~@body))))
+
+(defmacro not-empty-with [coll & with]
+ `(not-empty-let [name# ~coll]
+    (if (seq '~with) [~@with name#] name#)))
